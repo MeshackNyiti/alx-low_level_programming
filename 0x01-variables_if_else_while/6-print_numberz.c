4906a0be548818a00ed1d7f69981e7659d66356b
MeshackNyiti <meshackwnyiti@gmail.com>
2022-12-12 13:35:14 -0800

    print one to nine

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 /**
  * main - print all numbers from zero to nine
+ * Description: print all single digit number of base 10
+ * Starting from 0, followed by new line
  * Return: 0
  */
 int main(void)
 {
 	int c = 0;
 
-	while (c <= 10)
+	while (c < 10)
 	{
 		putchar(48 + c);
 		c++;
