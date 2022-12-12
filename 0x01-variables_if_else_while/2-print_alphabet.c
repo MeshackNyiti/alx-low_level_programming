335017ce225889c3aa2635f4259c9581a841f75d
MeshackNyiti <meshackwnyiti@gmail.com>
2022-12-12 11:24:51 -0800

    print a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Get a random it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
