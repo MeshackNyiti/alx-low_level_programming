c6cc50e2e92ece05c741bd9388644346d206afff
MeshackNyiti <meshackwnyiti@gmail.com>
2022-12-14 09:58:25 -0800

    print a to z ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,20 +3,21 @@
 /**
  * print_alphabet_x10 - entry point
  *
- * Return: 0
+ * Return: x10 a-z
  */
 
 void print_alphabet_x10(void)
 {
-	int n = 0;
-	int c;
+	int n,c;
+	
+	n = 0;
 
 	while (n < 10)
 	{
-	for (c = 'a'; c <= 'z';c++)
-	{
-		_putchar (n);
-	}
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
 		n++;
 		_putchar('\n');
 	}
