849976aad30d2815ff3daaf5c3cb30bda220ef14
MeshackNyiti <meshackwnyiti@gmail.com>
2022-12-16 06:42:24 -0800

    multiplication between two parameters

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,9 +4,9 @@
  * mul - Multiplies two integer
  * @a: The first number to multiply
  * @b: The second number to multiply
- *
+ * Return: Multiplication between a and b
  */
 int mul(int a, int b)
 {
-	return (a*b);
+	return (a * b);
 }
